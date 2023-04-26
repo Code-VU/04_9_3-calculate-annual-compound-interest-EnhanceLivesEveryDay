82f86cca3157001876d026fe8d976d25b9da0400
EnhanceLivesEveryDay <hardestyhouse65202c@gmail.com>
2023-04-25 21:27:59 -0500

    pytested

--- compoundInterest.py
+++ compoundInterest.py
@@ -1,11 +1,37 @@
 def calculateCompoundInterest():
+    getLoanDetails()
+    print("---")
+    getLoanDetails()
+    print("---")
+    getLoanDetails()
     
-# This first 3 lines are provided for yougetACompoundInterest()
-# This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
- #print("Compound Interest: "+str(intrest))
+def getLoanDetails():
+    # This first 3 lines are provided for you 
+    # This first 3 lines are provided for you
+    error_msg = "Please enter numerical characters."
+    try:
+        client_one_principal = float(input("Principle (amount): "))
+    except:
+        print (error_msg)      
+    try: 
+        client_one_time = float(input("Time:               "))
+    except:
+        print (error_msg)  
+    try:    
+        client_one_rate = float(input("Rate:               "))
+    except:
+        print (error_msg)
+
+    ptr = (getACompoundInterest(client_one_principal, client_one_time, client_one_rate))
+    return ptr
+
+def getACompoundInterest(p, t, r):
+    r = r / 100 + 1
+    rtp = pow(r, t)
+    a = p * rtp
+    ci = a - p
+    interest = "Compound Interest: "+str(round(ci, 2))
+    print(interest)
 
     # end assignment
 
